0c6962c8f62e2b28c088bce673f7910393228ece
DeLeReaL <pavel.chirich@gmail.com>
2022-09-20 11:32:01 +0300

    PMSHelper v 0.3

--- src/main/kotlin/data/remote/RetrofitClient.kt
+++ src/main/kotlin/data/remote/RetrofitClient.kt
@@ -1,13 +1,15 @@
 package data.remote
 
+import retrofit2.Retrofit
+
 private const val WEATHER_BASE_URL = "http://api.weatherapi.com/v1/"
 private const val REVERSE_GEOCODER_BASE_URL = "http://nominatim.openstreetmap.org"
 private const val API_KEY = "892bf1b294634ca4bf872724222009"
 
 class RetrofitClient {
 
-    fun getClient():Retrofit{
-
+    fun getClient(): Retrofit{
+        
     }
 
 }
