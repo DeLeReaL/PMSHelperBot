0ee58a7ccc1efc50a7105aa667ca93b2ee9a6629
DeLeReaL <pavel.chirich@gmail.com>
2022-09-20 10:35:36 +0300

    PMSHelper v 0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    //bot api
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.6")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
@@ -26,4 +37,12 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
---
+++ src/main/kotlin/data/remote/RetrofitClient.kt
@@ -0,0 +1,13 @@
+package data.remote
+
+private const val WEATHER_BASE_URL = "http://api.weatherapi.com/v1/"
+private const val REVERSE_GEOCODER_BASE_URL = "http://nominatim.openstreetmap.org"
+private const val API_KEY = "892bf1b294634ca4bf872724222009"
+
+class RetrofitClient {
+
+    fun getClient():Retrofit{
+
+    }
+
+}
